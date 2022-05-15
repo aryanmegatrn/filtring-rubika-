2d1c9286660fbc0a9699335384224328f345f7ef
aryanmegatrn <105610770+aryanmegatrn@users.noreply.github.com>
2022-05-16 03:15:33 +0430

    Update and rename README.md to filtring.py

---
+++ filtring.py
@@ -0,0 +1,194 @@
+# filtring-rubika
+# salam ❤️
+#almaytm                               
+
+import os
+# almayt
+os.system("pip install colored")
+os.system("pip install colorama")
+os.system("pip install datetime")
+#almayt
+import time                                                     
+import sys                                                      
+import colored                                                  
+import colorama                                                 
+import datetime
+from colored import fg, bg, attr
+from colorama import Fore, Back, Style                          
+# code colors
+red='\033[31m'
+green='\033[32m'
+blue='\033[36m'
+pink='\033[35m'
+rang='\033[34m'
+#$CRIPT
+
+os.system("clear")
+
+print(f"{green} ")
+
+print (Fore.WHITE + "")
+
+#date()
+x = str(datetime.datetime.now())
+
+print("started " + (x))
+
+print(f"{blue} ")
+print ("         Script By")
+print (Fore.YELLOW + "")
+x = """ mojtaba cassifer bot running....
+.                                                      .
+      .n                     .             .                n.
+  . .dP                   dP               9b               9b   .
+ 4  qXb         .        dX                 Xb       .      dXp   t
+dX.  9Xb      .dXb     __                     __    dXb.   dXP   .Xb
+9XXb._     _.dXXXXb dXXXXbo.               dXXXXb dXXXXb._   _.dXXP
+ 9XXXXXXXXXXXXXVXXXXXXXXOo.           .oOXXXXXXXXVXXXXXXXXXXXXXXXP
+   9XXXXXXXXXXXXXXX ~   ~ OOO8b   d8OOO ~   ~ XXXXXXXXXXXXXXXXXP
+     9XXXXXP   9XX      *     98v8P      *     XXP   9XXXXXXXP
+      ~~~       9X.          .db|db.          .XP       ~~~
+                  )b.  .dbo.dP' v  9b.odb.  .dX(
+                ,dXXXXXXXXXXXb     dXXXXXXXXXXXb.
+               dXXXXXXXXXXXP'   .    9XXXXXXXXXXXb
+              dXXXXXXXXXXXXb   d|b   dXXXXXXXXXXXXb
+              9XXb'    XXXXXb.dX|Xb.dXXXXX'    dXXP
+                       9XXXXXX(   )XXXXXXP
+                       XXXX X. v .X XXXX
+                        XP^X' b   d' X^XX
+                        X. 9         P )X
+                         b          '  d'
+"""
+for c in x:
+    sys.stdout.write(c)
+    sys.stdout.flush()
+    time.sleep(0.02)
+#⁶6⁶
+print(f"{red} ")
+d = """
+ ❤️❤️❤️❤️❤️💙💜💙💚💚💜💙💜💙💜💜
+happy my birthday  😐❤️💙
+                            ⁶6⁶
+          💚💚💚💚💚💚
+         💜💜💜💜💜💜💜
+       💙💙💙💙💙💙💙💙💙
+    ❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️
+                     !!!! Script is free !!!!
+                   almayt😐❤️ almaytm ☘️
+          //////almaytm >~❤️💚💜💛💙 //////////////////////////////////////////////////////////////////////////.......
+"""
+for d in d:
+        sys.stdout.write(d)
+        sys.stdout.flush()
+        time.sleep(0.03)
+# Mjtba *_*
+#my cat
+#   ️❤️❤️❤️❤️❤️❤️
+#   💙💙💙💙💙💙      
+#   💚💚💚💚💚💚  
+#   💜💜💜💜💜💜
+
+time.sleep(4.3)
+print(f"{green} ")
+print ("ok welcome friends . ❤️ . ")
+print (Fore.YELLOW + "")
+time.sleep(1.7)
+print ("                       ID RUBIKA mi_aryan_mega")
+print ("                         ID telegram mi_aryan_megatrn")
+print ("                          New Id rubika : aryan_megatrn")
+print(f"{blue} ")
+time.sleep(1.0)
+print ("__________")
+time.sleep(0.7)
+print ("#_________")
+time.sleep(0.6)
+print ("##________")
+time.sleep(0.5)
+print ("###_______")
+time.sleep(0.4)
+print ("####______")
+time.sleep(0.3)
+print ("#####_____")
+time.sleep(0.2)
+print ("######____")
+time.sleep(0.1)
+print ("#######___")
+time.sleep(0.5)
+print ("########__")
+time.sleep(0.1)
+print ("#########_")
+time.sleep(0.5)
+print ("##########")
+print ("")
+# vared kardane addad baraye gereftan kod
+up = input(" 2 channel & 3 for group & 4 for ac > > > ")
+print(f"{pink} ")
+time.sleep(3.0)
+print("""
+  update =
+$        cd filtring-rubika.py
+$       python Filtring.py
+$       git clone https://github.com/aryanmegatrn/filtring-rubika
+""")
+
+print(f"{red} ")
+
+
+
+if up == "2":
+
+        print ('code for Channel ≠>' + '%s%s(https://xexx.4.7.4.7.4.6.3.7.9.6.8.4.8.5.7.8.5.5.7.8.4.5.5.4.6.4.7.9.5.7.9.5.6.8.5.7.9)%s' % (fg('red'), bg('yellow'), attr('reset')))
+
+
+if up != "2":
+        print("error - lotfan dorost vared konid")
+
+
+if up == "3":
+        print ('code group ≠>' + '%s%s(https://.4.6.3.6.4.6.8.3.5.8.4.7.9.4.2.8.3.7.9.5.8.0.¥./8.2.0.9/f////g.h/5.1.5.8.8.0.1.4.0.6.1.5.0.7.3.7.0.4.6.8.6/)%s' % (fg('white'), bg('black'), attr('reset')))
+
+
+if up != "3":
+
+        print("error - lotfan dorost vared konid")
+
+
+if up == "4":
+
+        print ('code for account ≠>' + '%s%s (5.6.3.6.7.4.7.4.6.7.4.6.8.4.7.7.x.e.x.e.xx4.6.7.6.7.4.6.7.4.3.5.7.4.7.8.4.3.6)%s' % (fg('red'), bg('yellow'), attr('reset')))
+
+
+
+if up != "4":
+        print("error - lotfan dorost vared konid")
+
+time.sleep(0.6)
+print(f"{green}" )
+print ("""
+Kanal haye rubikam =
+almaytm - almaytm
+almaytm - almaytm
+ """)
+print("""
+link gap rubikam ==>>>
+https://rubika.ir/lockader_aryan_megatrn""")
+print (Fore.WHITE + " ")
+time.sleep(3.0)
+print (" Bye Bye My Friend :)🙂❤️")
+time.sleep(3.0)
+print (f"{red} ")
+time.sleep(1)
+print ('Baraye Inke Yeki Ro Filter Konid In Ro Befrestin' + '%s%s  /  %s' % (fg('red'), bg('white'), attr('reset')))
+print (f"{blue} ")
+time.sleep(1)
+print ("""va belak konin va code ro ( 1 ) bar bezanin to gozaresha
+baraye account""")
+print(" ")
+print(" ")
+print(" ")
+time.sleep(2.0)
+os.system("cd $HOME")
+os.system("rm -rf /data/data/com.termux/files/home/Filtering-Rubika && git clone https://github.com/C4ssif3r/Filtering-Rubika")
+print ("time end :")
+print (Fore.WHITE + "")
+date()
